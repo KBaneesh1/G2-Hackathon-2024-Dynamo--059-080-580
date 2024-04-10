1a349bafc100ff378938fcdc5169937667adffd9
ANEESH K B <kbaneesh2002@gmail.com>
2024-04-10 13:39:14 +0530

    update automation

--- api_test.py
+++ api_test.py
@@ -8,21 +8,35 @@ headers = {
     'Authorization': 'Bearer {0}'.format(api_token)
 }
 
-resp = requests.get(url, headers=headers)
-if resp.status_code == 200:
-    data = resp.json()
+all_products = []
+count = 0
+next_page_url = url
+while next_page_url:
+    if count >= 10:
+        break
+    resp = requests.get(next_page_url, headers=headers)
+    if resp.status_code == 200:
+        data = resp.json()
+        all_products.extend(data['data'])
 
-    df = pd.DataFrame(data['data'])
-    pd.set_option('display.max_columns', None)
-    pd.set_option('display.expand_frame_repr', False)
-    pd.set_option('max_colwidth', None)
+        next_page_url = data['links'].get('next')
+    else:
+        print("Failed to retrieve valid response from the API")
+        break
+    count += 1
 
-    # Display the DataFrame
-    print(df)
-    # Writing JSON data to a file with indentation of 4
-    with open('response_data.json', 'w') as json_file:
-        json.dump(data, json_file, indent=4)
+df = pd.DataFrame(all_products)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.expand_frame_repr', False)
+pd.set_option('max_colwidth', None)
 
-    print("JSON response saved to response_data.json")
-else:
-    print("Failed to retrieve valid response from the API")
+# Display the DataFrame
+print(df)
+
+# Writing JSON data to a file with indentation of 4
+with open('response_data.json', 'w') as json_file:
+    json.dump(all_products, json_file, indent=4)
+
+print("JSON response saved to response_data.json")
+
+# i gave got 10 pag
---
+++ pipeline.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+# Set up the WebDriver
+driver = webdriver.Chrome()
+product_rate=[]
+count = 1
+
+def func():
+    pass
+# Navigate to the website
+
+total = 42
+try:
+    while True:
+        if(count<=total):
+            driver.get(f"https://www.getapp.com/project-management-planning-software/page-{count}/")
+            # time.sleep(1)
+                # Wait for the repositories to be present
+                # Find all repository links
+            prod_links = driver.find_elements(By.XPATH, '//*[@id="__next"]/div[2]/div/div[2]/div[2]/div[2]')
+
+            for idx, prod in enumerate(prod_links):
+                print("inside for")
+                product = prod.find_element(By.XPATH, ".//div[1]/div/a")
+                prod_link = product.get_attribute("href")
+                prod_name = product.text
+                print(prod_name)
+                product.click()
+                WebDriverWait(driver, 2).until(EC.url_changes(driver.current_url))
+                func()
+                driver.back()
+            
+            count += 1
+        else:
+            break
+
+except Exception as e:
+    print(f"Error {e}")
