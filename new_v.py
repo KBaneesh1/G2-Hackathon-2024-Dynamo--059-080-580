e145e4d17e831beae20177dd1b181aca6d795641
ANEESH K B <kbaneesh2002@gmail.com>
2024-04-10 20:22:16 +0530

    new v

--- new_v.py
+++ new_v.py
@@ -1,45 +1,44 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import time
+from undetected_chromedriver import Chrome, ChromeOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-# Set up the WebDriver
-driver = webdriver.Chrome()
-product_rate=[]
+
+product_rate = []
 count = 1
 
 def func():
     pass
-# Navigate to the website
 
+# Set up the WebDriver
+options = ChromeOptions()
+options.headless = False  # Set to True if you don't want the browser GUI to show
+driver = Chrome(options=options)
+
+# Navigate to the website
 total = 42
+p = []
 try:
     while True:
-        if(count<=total):
+        if count <= total:
             driver.get(f"https://www.getapp.com/project-management-planning-software/project-management/page-{count}/")
-            # time.sleep(1)
-                # Wait for the repositories to be present
-                # Find all repository links
+
             prod_links = driver.find_elements(By.XPATH, '//*[@id="__next"]/div[2]/div/div[2]/div[2]/div[2]/div')
             prod_links = prod_links[1:25]
+            print(len(prod_link))
             for idx, prod in enumerate(prod_links):
                 print("inside for")
                 product = prod.find_element(By.XPATH, ".//div[1]/div/a")
                 prod_link = product.get_attribute("href")
                 prod_name = product.text
                 print(prod_name)    
-                # # product.click()
-                # child_state = driver.get(prod_link)
-                # WebDriverWait(driver, 2).until(EC.url_changes(driver.current_url))
-                func()
-                
-            
+                # func()
+                p.append(prod_link)
+
             count += 1
         else:
             break
 
 except Exception as e:
     print(f"Error {e}")
-
+print(p)
 driver.quit()
