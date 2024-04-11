c8b5584c87e0d7b7f6cb61ae9698a7a8120ca2e8
ANEESH K B <kbaneesh2002@gmail.com>
2024-04-11 11:01:41 +0530

    added wwebsite

--- new_v.py
+++ new_v.py
@@ -17,34 +17,44 @@ driver = Chrome(options=options)
 
 # Navigate to the website
 total = 42
-p = {"links":[]}
+# p = {"links":[]}
 prod_link=''
-try:
-    while True:
-        if count <= total:
-            driver.get(f"https://www.getapp.com/project-management-planning-software/project-management/page-{count}/")
+with open('products_website.jsonl','a') as json_file:
+    try:
+        while True:
+            if count <= total:
+                driver.get(f"https://www.getapp.com/project-management-planning-software/project-management/page-{count}/")
 
-            prod_links = driver.find_elements(By.XPATH, '//*[@id="__next"]/div[2]/div/div[2]/div[2]/div[2]/div')
-            prod_links = prod_links[1:25]
-            print(len(prod_link))
-            for idx, prod in enumerate(prod_links):
-                print("inside for")
-                product = prod.find_element(By.XPATH, ".//div[1]/div/a")
-                prod_link = product.get_attribute("href")
-                prod_name = product.text
-                print(prod_name)    
-                # func()
-                p["links"].append(prod_link)
+                prod_links = driver.find_elements(By.XPATH, '//*[@id="__next"]/div[2]/div/div[2]/div[2]/div[2]/div')
+                # prod_links = prod_links[1:25]
+                print(len(prod_link))
+                for idx, prod in enumerate(prod_links):
+                    print("inside for")
+                    try:
+                        product = prod.find_element(By.XPATH, ".//div[1]/div/a")
+                        website = prod.find_element(By.XPATH , ".//div[1]/div[2]/a")
+                        website_link = website.get_attribute("href")
+                        prod_link = product.get_attribute("href")
+                        prod_name = product.text
+                        print(prod_name)    
+                        # func()
+                        record = {
+                            "name":prod_name ,
+                            "product_link":prod_link,
+                            "website_link":website_link
+                        }
+                        json.dump(record,json_file)
+                    except:
+                        continue
+                count += 1
+            else:
+                break
 
-            count += 1
-        else:
-            break
-
-except Exception as e:
-    print(f"Error {e}")
+    except Exception as e:
+        print(f"Error {e}")
 print(p)
 driver.quit()
 
 # Write the list p to a JSON file
-with open('prod_links2.json', 'w') as json_file:
-    json.dump(p, json_file)
+# with open('prod_links2.json', 'w') as json_file:
+#     json.dump(p, json_file)
