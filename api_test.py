f4a7df0a0b7f7a3aa97d7a445e9030c7be972628
ANEESH K B <kbaneesh2002@gmail.com>
2024-04-12 09:39:02 +0530

    done

--- api_test.py
+++ api_test.py
@@ -11,31 +11,34 @@ headers = {
 all_products = []
 count = 0
 next_page_url = url
-with open('resp_data.jsonl', 'a') as json_file:
+with open('suiiiiii.jsonl', 'w',encoding='utf-8') as json_file:
     while next_page_url:
         try:
             resp = requests.get(next_page_url, headers=headers)
             if resp.status_code == 200:
+                k = ''''''
                 data = resp.json()
+                json_str=json.dumps(data)
+                print(type(json_str))
                 
-                df = pd.DataFrame(all_products)
-                pd.set_option('display.max_columns', None)
-                pd.set_option('display.expand_frame_repr', False)
-                pd.set_option('max_colwidth', None)
+                
+                json_file.write(json_str.strip()+"\n")
                 next_page_url = data['links'].get('next')
-                json.dump(data, json_file, indent=4)
-                json_file.write("\n")
             else:
                 print("Failed to retrieve valid response from the API")
                 break
             count += 1
-        except:
+        except Exception as e:
+            print(e)
             break
 
 
 # Display the DataFrame
 # print(df)
-
+ # df = pd.DataFrame(all_products)
+        # pd.set_option('display.max_columns', None)
+                # pd.set_option('display.expand_frame_repr', False)
+            # pd.set_option('max_colwidth', None)
 # Writing JSON data to a file with indentation of 4
 # with open('response_data.json', 'w') as json_file:
 #     json.dump(all_products, json_file, indent=4)
---
+++ scrpae.py
@@ -0,0 +1,75 @@
+from undetected_chromedriver import Chrome, ChromeOptions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import json
+
+product_rate = []
+count = 1
+
+options = ChromeOptions()
+options.headless = False  # Set to True if you don't want the browser GUI to show
+driver = Chrome(options=options)
+
+# Navigate to the website
+total = 0
+# p = {"links":[]}
+prod_link=''
+r = 1
+c = 1
+# //*[@id="__next"]/div[1]/div/div[4]/div[7]/div[1]/h2
+with open(f'collaboration.jsonl','a') as json_file:
+    try:
+        while True:
+            count = 1
+            if c > 18 and r == 2:
+                break
+            if c > 18:
+                c = 1
+                r = 2
+            if r == 2 and c == 1:
+                continue
+            driver.get(f"https://www.getapp.com/browse/")
+            browsing = driver.find_element(By.XPATH, f'//*[@id="__next"]/div[1]/div/div[4]/div[7]/div[2]/div[{r}]/a[{c}]').get_attribute("href")
+            c += 1
+            webi = driver.get(f"{browsing}/page-1/")
+            total = driver.find_element(By.XPATH, f'//*[@id="__next"]/div[2]/div/div[2]/div[2]/div[2]/div[27]/p').text
+            total = total.split(" ")[-1]
+            print("total",total)
+            total = int(total)
+
+            while True:
+                if count <= total:
+                    driver.get(f"{browsing}/page-{count}/")
+
+                    prod_links = driver.find_elements(By.XPATH, '//*[@id="__next"]/div[2]/div/div[2]/div[2]/div[2]/div')
+                    # prod_links = prod_links[1:25]
+                    # print(len(prod_link))
+                    for idx, prod in enumerate(prod_links):
+                        # print("inside for")
+                        try:
+                            product = prod.find_element(By.XPATH, ".//div[1]/div/a")
+                            website = prod.find_element(By.XPATH , ".//div[1]/div/div/div[1]/a")
+                            website_link = website.get_attribute("href")
+                            prod_link = product.get_attribute("href")
+                            prod_name = prod.find_element(By.XPATH, ".//div/div/div[1]/a/h2/span").text
+                            # print(prod_name)    
+                            # func()
+                            record = {
+                                "name":prod_name ,
+                                "product_link":prod_link,
+                                "website_link":website_link
+                            }
+
+                            json.dump(record,json_file)
+                        except:
+                            continue
+                    count += 1
+                else:
+                    print("done")
+                    break
+
+    except Exception as e:
+        print(f"Error {e}")
+# print(p)
+driver.quit()
